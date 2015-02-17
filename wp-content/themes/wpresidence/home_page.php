4f92516a9ecfa712b5168d07056b9baa5770909e
lameduz <mr.zenatimehdi@gmail.com>
2015-02-17 18:05:07 +0100

    Home++

--- wp-content/themes/wpresidence/home_page.php
+++ wp-content/themes/wpresidence/home_page.php
@@ -6,36 +6,30 @@ Template Name: Home Page
 ?>
 <?php get_header(); ?>
 <div class="row">
-	<h1>INSTANTIMMO EST LE PREMIER SITE D'ANNONCES IMMOBILIERES GEOLOCALISEES</h1>
-	<div class="col-md-4">
-		<div class="content-heading clearfix media">
-			<img src="<?php bloginfo('template_url'); ?>/img/assetsImmo/2-copie.png" class="img-thumbnail home_mid_img">
-			<h3>PUBLIEZ VOTRE ANNONCE</h3>
-			<p>Vous voulez vendre, mettre en location ou proposer une colocation ? Créez votre espace personnel en quelques clics et déposez gratuitement votre annonce...</p>
+		<h1>INSTANTIMMO EST LE PREMIER SITE D'ANNONCES IMMOBILIERES GEOLOCALISEES</h1>
+		<div class="col-md-3">
+			<div class="content-heading clearfix media block_padding-l">
+				<img src="<?php bloginfo('template_url'); ?>/img/assetsImmo/2-copie.png" class="img-thumbnail home_mid_img ">
+				<h3 class="home_h3_style">PUBLIEZ VOTRE ANNONCE</h3>
+				<p class="home_p_style">Vous voulez vendre, mettre en location ou proposer une colocation ? Créez votre espace personnel en quelques clics et déposez gratuitement votre annonce...</p>
+			</div>
 		</div>
-	</div>
 
-	<div class="col-md-4">
-		<div class="content-heading clearfix media">
-			<img src="<?php bloginfo('template_url'); ?>/img/assetsImmo/carte.png" class="img-thumbnail home_mid_img">
-			<h3>VOUS RECHERCHEZ</h3>
-			<p>Un appartement en centre ville, une maison dans un quartier pavillonnaire... Un parking pour votre voiture ou un terrain pour faire construire... Cherchez et trouvez parmi des milliers d'annonces de particuliers et de professionnels...</p>
-			<h4>En un instant!</h4>
+		<div class="col-md-4 home_side_marge">
+			<div class="content-heading clearfix media block_padding">
+				<img src="<?php bloginfo('template_url'); ?>/img/assetsImmo/carte.png" class="img-thumbnail home_mid_img">
+				<h3 class="home_h3_style">VOUS RECHERCHEZ</h3>
+				<p class="home_p_style">Un appartement en centre ville, une maison dans un quartier pavillonnaire... Un parking pour votre voiture ou un terrain pour faire construire... Cherchez et trouvez parmi des milliers d'annonces de particuliers et de professionnels...</p>
+				<h4>En un instant!</h4>
+			</div>
 		</div>
-	</div>
-	<div class="col-md-4">
-		<div class="content-heading clearfix media">
-			<img src="<?php bloginfo('template_url'); ?>/img/assetsImmo/Calque-50.png" class="img-thumbnail home_mid_img">
-			<h3>BESOIN D'AIDE POUR VOTRE PROJET ?</h3>
-			<p>Pour vous accompagner dans votre projet retrouvez les coordonnées de tous les professionnels de l'immobilier près de chez vous...</p>
+		<div class="col-md-3">
+			<div class="content-heading clearfix media block_padding-r">
+				<img src="<?php bloginfo('template_url'); ?>/img/assetsImmo/Calque-50.png" class="img-thumbnail home_mid_img">
+				<h3 class="home_h3_style">BESOIN D'AIDE POUR VOTRE PROJET ?</h3>
+				<p class="home_p_style">Pour vous accompagner dans votre projet retrouvez les coordonnées de tous les professionnels de l'immobilier près de chez vous...</p>
+			</div>
 		</div>
-	</div>
-
-	<div class="col-md-4">
-		
-		
-	</div>
-	
 </div>
 
 <?php get_footer();?>
