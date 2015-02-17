424ca2894de812871a0c74761e454cdd99f1ba42
lameduz <mr.zenatimehdi@gmail.com>
2015-02-17 17:02:16 +0100

    CSS Mehdi

--- wp-content/themes/wpresidence/home_page.php
+++ wp-content/themes/wpresidence/home_page.php
@@ -6,13 +6,24 @@ Template Name: Home Page
 ?>
 <?php get_header(); ?>
 <div class="row">
-	<div class="container">
-		<div class="col-md-12 col-sm-8 col-xs-12">
 			<h1>INSTANTIMMO EST LE PREMIER SITE D'ANNONCES IMMOBILIERES GEOLOCALISEES</h1>
-			<img src="<?php bloginfo('template_url'); ?>/img/assetsImmo/2-copie.png" class="img-thumbnail">
-			<img src="<?php bloginfo('template_url'); ?>/img/assetsImmo/carte.png" class="img-thumbnail">
+		<div class="col-md-4">
+			<img src="<?php bloginfo('template_url'); ?>/img/assetsImmo/2-copie.png" class="img-thumbnail home_mid_img">
+			<p>PROAZIDIJIAdazzzzzzzzzzzzzzzzzziadjiajizdhiazhdiddddddddddddddddddddZDJI</p>
 		</div>
-	</div>
+			
+			
+		<div class="col-md-4">
+		<img src="<?php bloginfo('template_url'); ?>/img/assetsImmo/carte.png" class="img-thumbnail home_mid_img">
+		<div class="row">
+				<p>PROAZIDIJIAZDJI</p>
+			</div>
+		</div>
+		<div class="col-md-4">
+		<img src="<?php bloginfo('template_url'); ?>/img/assetsImmo/Calque-50.png" class="img-thumbnail home_mid_img">
+		
+		</div>
+	
 </div>
 
 <?php get_footer();?>
