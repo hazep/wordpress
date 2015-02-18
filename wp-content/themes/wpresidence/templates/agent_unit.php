42bfeeba2f12f787f81664d1600b7fb6f56a87fb
youcefchibani <chibani.epitech@gmail.com>
2015-02-18 19:17:46 +0100

    agent_youss

--- wp-content/themes/wpresidence/templates/agent_unit.php
+++ wp-content/themes/wpresidence/templates/agent_unit.php
@@ -9,7 +9,6 @@ $agent_phone        = esc_html( get_post_meta($post->ID, 'agent_phone', true) );
 $agent_mobile       = esc_html( get_post_meta($post->ID, 'agent_mobile', true) );
 $agent_email        = esc_html( get_post_meta($post->ID, 'agent_email', true) );
 
-
 if (function_exists('icl_translate') ){
     $agent_posit      =   icl_translate('wpestate','agent_position', esc_html( get_post_meta($post->ID, 'agent_position', true) ) );
 }else{
@@ -20,9 +19,8 @@ $agent_facebook     = esc_html( get_post_meta($post->ID, 'agent_facebook', true)
 $agent_twitter      = esc_html( get_post_meta($post->ID, 'agent_twitter', true) );
 $agent_linkedin     = esc_html( get_post_meta($post->ID, 'agent_linkedin', true) );
 $agent_pinterest    = esc_html( get_post_meta($post->ID, 'agent_pinterest', true) );
-$name               = get_the_title();
+$name               = get_the_title(11);
 $link               = get_permalink();
-
 $extra= array(
         'data-original'=>$preview[0],
         'class'	=> 'lazyload img-responsive',    
@@ -49,11 +47,16 @@ if($options['content_class']=='col-md-12'){
         </div>
 
         <div class="col-md-6">
-            <p><?php print $agent_mobile; ?></p>
-            <p><?php print $agent_email; ?></p>
-            <p><?php print $agent_skype; ?></p>
-            <p><?php print $agent_position; ?></p>
-            <p><?php print $agent_twitter; ?></p>
+            <p class="agent_info"><?php print $name; ?></p>
+            <p class="agent_info"><?php print $agent_position; ?></p>
+            <p class="agent_info"><?php print $agent_mobile; ?></p>
+            <p class="agent_info"><?php print $agent_phone; ?></p>
+            <p class="agent_info"><?php print $agent_email; ?></p>
+            <p class="agent_info"><?php print $agent_skype; ?></p>
+            <p class="agent_info"><?php print $agent_twitter; ?></p>
+            <p class="agent_info"><?php print $agent_facebook; ?></p>
+            <p class="agent_info"><?php print $agent_pinterest; ?></p>
+            <p class="agent_info"><?php print $agent_linkedin; ?></p>
         </div>
 
     </div>
