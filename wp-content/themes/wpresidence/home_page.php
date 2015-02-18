9f24aa53f393e9c350cd172973c9dac8c2c00407
lameduz <mr.zenatimehdi@gmail.com>
2015-02-18 16:15:29 +0100

    HOME PAGE MANQUE HOVER

--- wp-content/themes/wpresidence/home_page.php
+++ wp-content/themes/wpresidence/home_page.php
@@ -5,39 +5,96 @@ Template Name: Home Page
 */
 ?>
 <?php get_header(); ?>
-<div class="row">
-	
+<div class="container-fluid container_padding">
+	<div class="row">
+
 		<h1 id="home_main_title">INSTANTIMMO EST LE PREMIER SITE D'ANNONCES IMMOBILIERES GEOLOCALISEES</h1>
 		
 		
-		<div class="col-md-4">
+		<div class="col-md-4 col-sm-8 col-xs-3">
 			
-				<img src="<?php bloginfo('template_url'); ?>/img/assetsImmo/2-copie.png" class="img-thumbnail home_mid_img aligncenter ">
-				<h3 class="home_h3_style">PUBLIEZ VOTRE ANNONCE</h3>
-				<p class="home_p_style aligncenter">Vous voulez vendre, mettre en location ou proposer une colocation ? Créez votre espace personnel en quelques clics et déposez gratuitement votre annonce...</p>
+			<img src="<?php bloginfo('template_url'); ?>/img/assetsImmo/2-copie.png" class="img-thumbnail home_mid_img aligncenter ">
+			<h3 class="home_h3_style">PUBLIEZ VOTRE ANNONCE</h3>
+			<p class="home_p_style aligncenter">Vous voulez vendre, mettre en location ou proposer une colocation ? Créez votre espace personnel en quelques clics et déposez gratuitement votre annonce...</p>
 			
 		</div>
 
-		<div class="col-md-4 home_side_marge">
+		<div class="col-md-4 col-sm-8 col-xs-3 home_side_marge">
 			<div class="content-heading clearfix media">
 				<img src="<?php bloginfo('template_url'); ?>/img/assetsImmo/carte.png" class="img-thumbnail home_mid_img aligncenter">
 				<h3 class="home_h3_style">VOUS RECHERCHEZ</h3>
 				<p class="home_p_style aligncenter">Un appartement en centre ville, une maison dans un quartier pavillonnaire... Un parking pour votre voiture ou un terrain pour faire construire... Cherchez et trouvez parmi des milliers d'annonces de particuliers et de professionnels...</p>
-				<h4 class="home_h3_style">En un instant!</h4>
+				<h4 class="home_h3_style" id="un_instant">En un instant!</h4>
 			</div>
 		</div>
-		<div class="col-md-4">
+
+		<div class="col-md-4 col-sm-8 col-xs-3">
 			<div class="content-heading clearfix media">
 				<img src="<?php bloginfo('template_url'); ?>/img/assetsImmo/Calque-50.png" class="img-thumbnail home_mid_img aligncenter">
 				<h3 class="home_h3_style">BESOIN D'AIDE POUR VOTRE PROJET ?</h3>
 				<p class="home_p_style aligncenter">Pour vous accompagner dans votre projet retrouvez les coordonnées de tous les professionnels de l'immobilier près de chez vous...</p>
 			</div>
 		</div>
-	
-		
-	
+	</div>
 </div>
 
+
+
+<div class="row container_padding_2 home_square_bg">
+	<div class="col-md-6 col-sm-8 col-xs-3">
+		<div class="media">
+			<div class="media-body media_body_marge-b">
+				<a class="pull-right" href="#">
+					<img class="media-object img-circle media_padding_l" src="<?php bloginfo('template_url');?>/img/assetsImmo/Calque-45.png" alt="Media Object">
+				</a>
+				<h4 class="media-heading text_align_r">AGENCES IMMOBILIERES & INDEPENDANTS</h4>
+				<p class="text_align_r home_p_style_2">Besoin de conseils, d’une estimation de votre logement, vous n’avez pas le temps pour vous occuper de votre recherche ? Les professionnels près de chez vous se feront un plaisir de vous accompagner pour mener à bien votre projet</p>
+				<a class="buttons alignright" href="#">Rechercher</a>
+
+			</div>
+		</div>
+	</div>
+	<div class="col-md-6 col-sm-8 col-xs-3">
+		<div class="media">
+			<div class="media-body media_body_marge-b">
+				<a class="pull-left" href="#">
+					<img class="media-object img-circle media_padding_r" src="<?php bloginfo('template_url');?>/img/assetsImmo/Calque-44.png" alt="Media Object">
+				</a>
+				<h4 class="media-heading head_align_l">DIAGNOSTIQUEURS</h4>
+				<p class="home_p_style_2">Des diagnostics sont obligatoires pour la vente ou mise en location de votre bien immobilier. DPE, électricité, gaz, amiante, plomb, Loi Carrez, termites, ERNT… Retrouvez tous les professionnels spécialisés et habilités près de chez vous.</p>
+				<a class="buttons" href="#">Rechercher</a>
+
+			</div>
+		</div>
+	</div>
+	<div class="col-md-6 col-sm-8 col-xs-3">
+		<div class="media">
+			<div class="media-body media_body_marge-b">
+				<a class="pull-right" href="#">
+					<img class="media-object img-circle media_padding_l" src="<?php bloginfo('template_url');?>/img/assetsImmo/Calque-42.png" alt="Media Object">
+				</a>
+				<h4 class="media-heading text_align_r">COURTIERS</h4>
+				<p class="text_align_r home_p_style_2">Vous avez trouvé le bien tant recherché, les démarches auprès des banques vous paraissent trop compliquées ? Des experts en financement sont à votre disposition pour finaliser votre projet.</p>
+				<a class="buttons alignright" href="#">Rechercher</a>
+
+			</div>
+		</div>
+	</div>
+	<div class="col-md-6 col-sm-8 col-xs-3">
+		<div class="media">
+			<div class="media-body media_body_marge-b">
+				<a class="pull-left" href="#">
+					<img class="media-object img-circle media_padding_r loupe" src="<?php bloginfo('template_url');?>/img/assetsImmo/Calque-43.png" alt="Media Object">
+				</a>
+				<h4 class="media-heading">IMMOBILIER NEUF</h4>
+				<p class="home_p_style_2">Envie d’un logement neuf ? Vous voulez investir, défiscaliser ? Faire construire votre maison ? Consulter tous les programmes neufs et maisons à bâtir dans la zone de recherche sélectionnée.</p>
+				<a class="buttons" href="#">Rechercher</a>
+			</div>
+		</div>
+	</div>
+	<div>
+	<h3 class="home_h3_style" id="you_pro">Vous êtes un professionnel ? Cliquez ici ! </h3>
+</div>
 <?php get_footer();?>
 
 
