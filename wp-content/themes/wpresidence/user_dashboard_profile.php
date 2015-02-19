5ab7186561df58051983899b599fb3ba0de599ca
davchichi4 <etienne.daviot@epitech.eu>
2015-02-19 19:01:10 +0100

    maj

--- wp-content/themes/wpresidence/user_dashboard_profile.php
+++ wp-content/themes/wpresidence/user_dashboard_profile.php
@@ -130,20 +130,20 @@ $options=wpestate_page_details($post->ID);
 <!--        -               -->
 
 <div class="row background_profil">
-    <div class="col-md-2">
+    <div class="col-lg-2 col-md-2">
        <div class="sides">
         <?php generated_dynamic_sidebar( $options['sidebar_name']);  ?>
        </div>
     </div>  
     
     
-    <div class="col-md-8 background_profil_content">
+    <div class="col-lg-8 col-md-8 background_profil_content">
         
         <?php get_template_part('templates/ajax_container'); ?>
         
            <?php   get_template_part('templates/user_profile'); ?>
     </div>
-    <div class="col-md-2">
+    <div class="col-lg-2 col-md-2">
       <h3 class="gras calend">CALENDRIER</h3>
       <hr class="border_black">
       <?php the_widget( 'WP_Widget_Calendar'); ?>
